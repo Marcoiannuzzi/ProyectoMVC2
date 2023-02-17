3c4ea836c46b207d4b0a1781692a0e4899713282
Marco Iannuzzi <marcoiannuzzi@live.com.ar>
2023-02-17 12:29:38 -0300

    cambios en vista portfolio

---
+++ Models/ProyectosModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoMVC2.Models
+{
+    public class ProyectosModel
+    {
+        public string NombreProyecto { get; set; }
+        public string Categoria { get; set;}
+        public string Categorias { get; set;}
+        public string Imagen { get; set;}
+    }
+}
